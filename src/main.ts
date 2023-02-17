e2715e6901b31b58ab106f4b6f343a29c43ade50
Feng Y <fengy142857@gmail.com>
2023-02-17 09:54:14 +0100

    can fetch and display correctly

--- src/main.ts
+++ src/main.ts
@@ -14,22 +14,24 @@ const getMob = async () => {
 	console.log(mobName);
 
 	const getMembersData = await fetch(
-		'https://lab-mobster-backend-production.up.railway.app/mobs/1676464991028/members'
+		'https://lab-mobster-backend-production.up.railway.app/members/'
 	);
 	const members = await getMembersData.json();
 	console.log(members.mobMembers);
 
 	document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <section class="main-section"
-    <h2 class="main-section__main-title">${mobName.mobs.mobs[1]}</h2>
+  <section class="main-section">
+    <h2 class="main-section__main-title">${mobName.mobs.mobs[0].name}</h2>
     <ul class="main-section__list">
-      <li class="main-section__list__element">${members.mobMembers.membersList[0]}</li>
-      <li class="main-section__list__element">${members.mobMembers.membersList[1]}</li>
-      <li class="main-section__list__element">${members.mobMembers.membersList[2]}</li>
-      <li class="main-section__list__element">${members.mobMembers.membersList[3]}</li>
+      <li class="main-section__list__element">${members.members[0].name}</li>
+      <li class="main-section__list__element">${members.members[1].name}</li>
+      <li class="main-section__list__element">${members.members[2].name}</li>
+      <li class="main-section__list__element">${members.members[3].name}</li>
     </ul>
   </section>
   `;
+  // document.querySelector<HTMLDivElement>('#app')!.innerHTML = members.members[0].name;
+ // document.querySelector<HTMLDivElement>('#app')!.innerHTML = mobName.mobs.mobs[0].name;
 };
 
 getMob();
