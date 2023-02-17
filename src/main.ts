6ba266c5b9ef0f3a1fc20501ae1605f233271ba5
salt-dev <salt-dev@salt-devs-MacBook-Pro.local>
2023-02-17 10:06:39 +0100

    Fix: Removed comments and console.logs

--- src/main.ts
+++ src/main.ts
@@ -7,19 +7,17 @@ import './style.css';
 // the fetch of the mob will only work with one mob because we didnt implement mobs/id
 
 const getMob = async () => {
-	const getMobData = await fetch(
-		'https://lab-mobster-backend-production.up.railway.app/mobs'
-	);
-	const mobName = await getMobData.json();
-	console.log(mobName);
+  const getMobData = await fetch(
+    'https://lab-mobster-backend-production.up.railway.app/mobs',
+  );
+  const mobName = await getMobData.json();
 
-	const getMembersData = await fetch(
-		'https://lab-mobster-backend-production.up.railway.app/members/'
-	);
-	const members = await getMembersData.json();
-	console.log(members.mobMembers);
+  const getMembersData = await fetch(
+    'https://lab-mobster-backend-production.up.railway.app/members/',
+  );
+  const members = await getMembersData.json();
 
-	document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <section class="main-section">
     <h2 class="main-section__main-title">${mobName.mobs.mobs[0].name}</h2>
     <ul class="main-section__list">
@@ -30,8 +28,6 @@ const getMob = async () => {
     </ul>
   </section>
   `;
-  // document.querySelector<HTMLDivElement>('#app')!.innerHTML = members.members[0].name;
- // document.querySelector<HTMLDivElement>('#app')!.innerHTML = mobName.mobs.mobs[0].name;
 };
 
 getMob();
